4f8d3c88e93514a3c5a5da59f41c299bf392db5c
Mark-Ball <markball111@gmail.com>
2020-04-09 13:32:51 +1000

    initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HeroFormComponent } from './hero-form/hero-form.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HeroFormComponent } 
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
